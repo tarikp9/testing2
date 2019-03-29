35e00202808903ef2c466bd42441122aed593782
tarikp9 <tarik-pasalic@hotmail.com>
2019-03-29 15:52:06 +0100

    message 2

--- sketch.test.js
+++ sketch.test.js
@@ -4,7 +4,7 @@ const sum = require("./sketch");
   expect(sum(1, 2)).toBe(3);
 });
  */
-test("adds 1 + 2 to equal 3", sumTest);
+test("adds 1 + 5 to equal 6", sumTest);
 
 function sumTest() {
   expect(sum(1, 5)).toBe(6);
